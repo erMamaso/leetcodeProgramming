aad6c6aa422eab6e342c38a888a4c53e3c064749
erMamaso <miguelangel.munoztubio@gmail.com>
2024-03-06 13:25:08 +0100

    Add files via upload

---
+++ 141. Linked List Cycle.cc
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fastPointer = head;
+        ListNode *slowPointer = head;
+        while(fastPointer != NULL && slowPointer != NULL && fastPointer->next != NULL){
+            fastPointer = fastPointer->next->next;
+            slowPointer = slowPointer->next;
+            if(fastPointer == slowPointer) return true;
+        }
+        return false;
+    }
+};
